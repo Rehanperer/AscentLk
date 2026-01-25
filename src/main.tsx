489073675b52cbbc44adf9e0886b783e62329436
Rehan Perera <pererarehan2007@gmail.com>
2026-01-25 21:32:22 +0300

    fix: switch root component from demo to landing page

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import ValorantDemo from './components/ValorantDemo'
 import './styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <ValorantDemo />
+        <App />
     </React.StrictMode>,
 )
