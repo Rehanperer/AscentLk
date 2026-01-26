e310a2cb037934d8af8943d528878d02aa6af962
Rehanperer <81005882+Rehanperer@users.noreply.github.com>
2026-01-26 04:21:07 +0000

    Deploying to gh-pages from @ Rehanperer/AscentLk@bcedefe4f357182ad616446c303f0966974911eb 🚀

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/AscentLk/sw.js', { scope: '/AscentLk/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
